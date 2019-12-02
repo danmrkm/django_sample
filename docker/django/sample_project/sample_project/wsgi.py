710478b25786385301d96aa42f73c6df2220bb75
danmrkm <38092962+danmrkm@users.noreply.github.com>
2019-12-03 03:19:47 +0900

    一部修正

--- docker/django/sample_project/sample_project/wsgi.py
+++ docker/django/sample_project/sample_project/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+import codecs
 from django.core.wsgi import get_wsgi_application
+
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample_project.settings')
 
